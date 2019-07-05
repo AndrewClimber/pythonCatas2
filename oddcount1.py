7493f4fe2b723a8518433bc9adadfab4d16b5657
AndrewClimber <seoclimber@gmail.com>
2019-07-05 11:41:18 +0700

    05.07.2019

--- oddcount1.py
+++ oddcount1.py
@@ -9,15 +9,18 @@ oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
 # aka
 def odd_count(n):
     return n//2
+
 # codewars
-def oddCount(n):
+def odd_count1(n):
     return n >> 1
 ####
-def odd_count(n):
+def odd_count2(n):
     return len(range(1, n, 2))
 
 ## what is this ??
-odd_count=2 .__rdiv__
+#odd_count3=2.__rdiv__
 
 ## what is this ??
-odd_count = (2).__rfloordiv__
+#odd_count4= (2).__rfloordiv__
+
+print(odd_count(15))
